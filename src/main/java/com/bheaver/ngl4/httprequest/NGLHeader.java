968ac13f71d2f6a659e1d7a5723b36c5e7eea633
Siddartha <sid.errabolu@gmail.com>
2018-04-16 07:24:09 -0400

    Updates

---
+++ src/main/java/com/bheaver/ngl4/httprequest/NGLHeader.java
@@ -0,0 +1,10 @@
+package com.bheaver.ngl4.httprequest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Value;
+
+@Data
+public class NGLHeader {
+    private String tenancyId;
+}
